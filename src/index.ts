cabd447df75b7c7cd5e0080122d51ddde8bc700e
willjw3 <will.j.ward3@gmail.com>
2021-04-11 20:54:34 +0900

    End of part 6

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
-import {createUserTable, createProjectTable, createAssignmentTable, addUser, addProject, addAssignment} from './utils'
 
-//createUserTable();
-//createProjectTable();
-//createAssignmentTable();
-//addUser('Farah Bennis', 'fbennis@zcorp.com', 'bbc123');
-//addProject('Onboard New Developers', 'active');
-//addAssignment('fbennis@zcorp.com', 1, 'Farah Bennis');
 
 
 const server = new ApolloServer({typeDefs, resolvers});
